be55ef5d4ff18ef16ad5b04d533b69923451104c
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-06-27 19:53:32 +0800

    Update working code

--- PyBank/pybank-testing.py
+++ PyBank/pybank-testing.py
@@ -5,12 +5,15 @@ import os
 import csv
 
 # Set path for file
-budget_csv = os.path.join("budget_data.csv")
+budget_csv = os.path.join("Resources","budget_data.csv")
 
 # Open the CSV file
 with open(budget_csv,'r') as csvfile:
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
+
+    csv_header = next(csvfile)
+    #print(f"CSV Header: {csv_header}")
 
 # Check the csv is being read
     #print(csvreader)
@@ -19,27 +22,32 @@ with open(budget_csv,'r') as csvfile:
         #print(row)
 
 # Set variables
-    total_months = 0
     
+    # Count of total months
+    total_months = 0
+
+    #Net profit/losses
+    profit_loss = 0
+
+# Calculate total number of months included in the dataset
     for row in csvreader:
         total_months = int(total_months+1)
 
-print(total_months)
+#print(total_months)
 
- # Calculate total number of months included in the dataset
+# Calculate net amount of profit/losses over the entire period
+    #for row in csvreader:
+        profit_loss = profit_loss+int(row[1])
 
-    #total_months = int(total_months+1)
-    #print(total_months)
+#print(profit_loss)
+    
+# Calculate average of the changes in profit/losses over the entire period
 
- # Calculate net amount of profit/losses over the entire period
 
-    #net_amount = int()
 
-     # Calculate average of the changes in profit/losses over the entire period
+# Calculate greatest increase in profits (date and amount) over the entire period
 
-    # Calculate greatest increase in profits (date and amount) over the entire period
-
-    # Calculate the greatest decrease in losses (date and amount) over the entire period
+# Calculate the greatest decrease in losses (date and amount) over the entire period
 
     # Print analysis to the terminal
     #print(f"{values_column}")
