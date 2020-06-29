6c1f1b17e980865dd7cf5d105263d1947ad1774e
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-06-29 21:22:03 +0800

    Add PyPoll test code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,19 +12,51 @@ with open(election_csv,'r') as csvfile:
 
     csv_header = next(csvfile)
     print(f"CSV Header: {csv_header}")
-
 #----------------------------------------------------------------------------------
-
 # Calculate the total number of votes cast
 
+# Create a list to store votes (assumes each vote in list is unique)
+    votes_list = []
 
+# Create a list to store candidates
+    candidates_list = []
+
+# Loop through each row in csv and add to votes list and candidates
+    for row in csvreader:
+        votes_list.append(row[0])
+        candidates_list.append(row[2])
+
+# Create a variable to hold number of votes
+    votes_count = int(len(votes_list))
+    # Check number of votes
+    # print(votes_count)
+
+#----------------------------------------------------------------------------------
 # Create complete list of candidates who received votes
 
+# Create a variable to hold unique candidate names (using sets)
+candidates_set = set(candidates_list)
+# Check unique candidate names
+# for x in candidates_set:
+#     print(x)
 
-# Calculate percentage of (total?) votes each candidate won
-
-
+#----------------------------------------------------------------------------------
 # Calculate total number of votes each candidate won
+
+# Create variables to hold number of votes for each candidate
+votes_khan = 0
+votes_correy = 0
+votes_li = 0
+votes_otooley = 0
+
+for x in candidates_list:
+    if candidates_list[x] == "Khan":
+        votes_khan + 1 
+
+print(votes_khan)
+
+# Calculate percentage of votes each candidate won
+
 
 
 # Determine the winner of the election based on popular vote (i.e. greatest percentage)
